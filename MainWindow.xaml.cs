e7d0d1d0ca3f9821af2c15db6bf619fab5e505ff
M4A1 <m4a1@vornex.xyz>
2026-05-23 19:52:47 -0300

    fixing some issues related to window visibility on the taskbar and removing keydown triggers.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,49 +23,25 @@ namespace Music
 
         private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);        
         [DllImport("user32.dll", SetLastError = true)] private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
-        public ViewMode CurrentViewMode { get; set; } = ViewMode.TaskBar;
 
         public MainWindow()
         {
             InitializeComponent();
             Loaded += MainWindow_Loaded;
-            KeyDown += MainWindow_KeyDown;
-            _timer = new DispatcherTimer();
-            _timer.Interval = TimeSpan.FromMilliseconds(200);
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(100)
+            };
             _timer.Tick += (s, _) =>
             {
-                var handle = new System.Windows.Interop.WindowInteropHelper(this).Handle;
-                SetWindowPos(handle, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE | SWP_SHOWWINDOW);
-            };            
-        }
-
-        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.L)
-            {
-                switch (CurrentViewMode)
+                try
                 {
-                    case ViewMode.TaskBar:
-                        PositionateWindow();                        
-                        break;
-
-                    case ViewMode.Window:
-                        PositionateTaskBar();                        
-                        break;
+                    var handle = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+                    SetWindowPos(handle, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE | SWP_SHOWWINDOW);
                 }
-            }
-            else if (e.Key == Key.H && Keyboard.Modifiers == ModifierKeys.Control)
-            {
-                // Ctrl+H - Abrir histórico
-                var historyWindow = new HistoryWindow();
-                historyWindow.Show();
-            }
-            else if (e.Key == Key.S && Keyboard.Modifiers == ModifierKeys.Control)
-            {
-                // Ctrl+S - Abrir configurações
-                var settingsWindow = new SettingsWindow();
-                settingsWindow.ShowDialog();
-            }
+                catch { }
+            };
+            _timer.Start();
         }
 
         private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -100,24 +76,6 @@ namespace Music
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            PositionateTaskBar();
-        }
-
-        private void PositionateWindow()
-        {
-            _timer.Stop();
-            Height = 82;
-            Left = 3;
-            try
-            {
-                Top = SystemParameters.WorkArea.Bottom - Height - 3;
-            }
-            catch { }
-            CurrentViewMode = ViewMode.Window;
-        }
-
-        private void PositionateTaskBar()
-        {
             Height = 42;
             Left = 3;
             try
@@ -125,14 +83,6 @@ namespace Music
                 Top = SystemParameters.WorkArea.Bottom - Height + 44.5;
             }
             catch { }
-            CurrentViewMode = ViewMode.TaskBar;
-
-            Dispatcher.BeginInvoke(() =>
-            {
-                var handle = new System.Windows.Interop.WindowInteropHelper(this).Handle;
-                SetWindowPos(handle, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE | SWP_SHOWWINDOW);
-                _timer.Start();
-            }, System.Windows.Threading.DispatcherPriority.Render);
         }
     }
 }
